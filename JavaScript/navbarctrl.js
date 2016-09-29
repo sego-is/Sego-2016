e6c2b1e05871c36863d16d48f058cc613a995911
stefand12 <stefand12@ru.is>
2016-09-29 23:49:40 +0000

    aukafileum eytt

--- JavaScript/navbarctrl.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('sego').controller('navbarCtrl', ['$scope', function ($scope) {
-	$scope.openSettings = function() {
-		console.log("settings");
-		$scope.templateURL = 'settings.html';
-	};
-}]);
