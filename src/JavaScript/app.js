10fbc02043475975b8dac74cd04c671cba62a432
Guðjón Pétursson <Gujn Ptursson>
2016-10-03 21:19:48 +0000

    timabundid veiw i gegnum kerfid

--- src/JavaScript/app.js
+++ src/JavaScript/app.js
@@ -3,11 +3,7 @@
 /*jslint browser:true */
 /* NewIrc + routeProvider config*/
 
-/*
- Hér er declerationið af NewIrc
- Einnig eru hér skigreind route'in sem við notum
- í NewIrc til að beina á controllera.
- */
+
 angular.module("sego", ['ngRoute', 'luegg.directives']).config(['$routeProvider',
     function ($routeProvider) {
         $routeProvider
