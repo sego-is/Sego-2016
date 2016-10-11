a85822cc107674523a0dc47fa28af62d0be8c587
Prumpu Rassgat <einaragusta@gmail.com>
2016-10-11 12:42:56 +0000

    nytt upphaf.. aftur

---
+++ app/scripts/directives/vidskiptavinirDirective.js
@@ -0,0 +1,12 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('segoApp')
+    .directive('vidskiptavinir', function() {
+    	return {
+        	templateUrl: '../../views/vidskiptavinir.html'
+        };
+    });
+})();
---
+++ app/scripts/services/auth.service.js
@@ -0,0 +1,61 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('segoApp')
+    .service('authService', authService);
+
+  authService.$inject = ['$rootScope', '$location', 'lock', 'authManager'];
+
+  function authService($rootScope, $location, lock, authManager) {
+
+    var userProfile = JSON.parse(localStorage.getItem('profile')) || {};
+    var authenticated = false;
+
+    function login() {
+      lock.show();
+    }
+
+    // Logging out just requires removing the user's
+    // id_token and profile
+    function logout() {
+      localStorage.removeItem('id_token');
+      localStorage.removeItem('profile');
+      authManager.unauthenticate();
+      userProfile = {};
+      authenticated = false;
+    }
+
+    function auth() {
+      return authenticated;
+    }
+
+    // Set up the logic for when a user authenticates
+    // This method is called from app.run.js
+    function registerAuthenticationListener() {
+      lock.on('authenticated', function(authResult) {
+        localStorage.setItem('id_token', authResult.idToken);
+        authManager.authenticate();
+        authenticated = true;
+
+        lock.getProfile(authResult.idToken, function(error, profile) {
+          if (error) {
+            console.log(error);
+          }
+          $location.path('/home');
+          localStorage.setItem('profile', JSON.stringify(profile));
+          $rootScope.$broadcast('userProfileSet', profile);
+        });
+      });
+    }
+
+    return {
+      userProfile: userProfile,
+      login: login,
+      logout: logout,
+      registerAuthenticationListener: registerAuthenticationListener,
+      auth: auth
+    };
+  }
+})();
