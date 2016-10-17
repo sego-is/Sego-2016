a2eb32f97edc7d9f21e5ec082d2aa02e315f0bd8
stefand12 <stefand12@ru.is>
2016-10-17 00:01:54 +0000

    stillingar verdbreytingar og klipparabreytingar

--- app/scripts/directives/stillingarDirective.js
+++ app/scripts/directives/stillingarDirective.js
@@ -1,29 +1,64 @@
-(function() {
+(function () {
 
   'use strict';
 
   angular
     .module('segoApp')
-    .directive('stillingar', function() {
-    	return {
+    .directive('stillingar', function () {
+      return {
         restrict: 'E',
-    		scope: {
-    			lokaGlugga: '&close'
-    		},
-        	templateUrl: '../../views/stillingar.html',
-        	link: function(scope, element, attrs) {
-        		scope.closeWindow = function() {
-        			scope.lokaGlugga();
-        		};
-                
-                scope.stillingar = {
-                    addKlippara: false
-                };
-                
-                scope.toggleKlippara = function() {
-                    scope.stillingar.addKlippara = !scope.stillingar.addKlippara;
-                }
-        	}
-        };
+        scope: {
+          lokaGlugga: '&close'
+        },
+        templateUrl: '../../views/stillingar.html',
+        link: function (scope, element, attrs) {
+          scope.closeWindow = function () {
+            scope.lokaGlugga();
+          };
+
+          scope.verdTrash = function () {
+            console.log("Henda verði");
+          };
+
+          scope.verdBreyting = function () {
+            console.log("breyta verði");
+          };
+
+          scope.klippTrash = function () {
+            console.log("Henda klippara");
+          };
+
+          scope.klippBreyting = function () {
+            console.log("breyta klippara");
+          };
+
+          scope.stillingar = {
+            klipp: true,
+            verd: false,
+            addKlippara: false,
+            addVerd: false
+          };
+
+          scope.toggleKlippara = function () {
+            if(scope.stillingar.verd) {
+              scope.stillingar.klipp = !scope.stillingar.klipp;
+              scope.stillingar.verd = !scope.stillingar.verd;
+              scope.stillingar.addKlippara = false;
+            } else {
+              scope.stillingar.addKlippara = !scope.stillingar.addKlippara;
+            }
+          };
+
+          scope.toggleVerdskra = function () {
+            if(scope.stillingar.klipp) {
+              scope.stillingar.verd = !scope.stillingar.verd;
+              scope.stillingar.klipp = !scope.stillingar.klipp;
+              scope.stillingar.addVerd = false;
+            } else {
+              scope.stillingar.addVerd = !scope.stillingar.addVerd;
+            }
+          }
+        }
+      };
     });
 })();
