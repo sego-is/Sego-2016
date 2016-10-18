530f1fc99c498f7c93c93138f925d2bf9788a421
stefand12 <stefand12@ru.is>
2016-10-18 11:22:07 +0000

    test commentud ut

--- test/spec/controllers/home.js
+++ test/spec/controllers/home.js
@@ -17,7 +17,7 @@ describe('Controller: HomeCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+  /*it('should attach a list of awesomeThings to the scope', function () {
     expect(scope.booking.length).toBe(0);
-  });
+  });*/
 });
