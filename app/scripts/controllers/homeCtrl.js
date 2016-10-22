1dfa10fa9ee2f162a2da73b6cc15e5f7b1ca0087
Prumpu Rassgat <einaragusta@gmail.com>
2016-10-22 10:17:05 +0000

    breyta local ip i out of the world ip

--- app/scripts/controllers/homeCtrl.js
+++ app/scripts/controllers/homeCtrl.js
@@ -21,7 +21,7 @@
 
         };
         
-        $http.get('dev.sego.is:6969/api/booking').then(function (response) {
+        $http.get('192.241.158.205:6969/api/booking').then(function (response) {
               console.log("GET RESPONSE:" + response);
             });
 
