ba2868ab53ae2c5755fa7dd27c34f16e0293e99a
stefand12 <stefand12@ru.is>
2016-10-23 11:04:40 +0000

    server call test

--- app/scripts/controllers/homeCtrl.js
+++ app/scripts/controllers/homeCtrl.js
@@ -1,97 +1,110 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    /**
-     * @ngdoc function
-     * @name segoEnnOgAfturApp.controller:AboutCtrl
-     * @description
-     * # AboutCtrl
-     * Controller of the segoEnnOgAfturApp
-     */
-    angular.module('segoApp')
-      .controller('HomeCtrl', ['$scope', 'dagatalFactory', '$http', function ($scope, dagatalFactory, $http) {
+  /**
+   * @ngdoc function
+   * @name segoEnnOgAfturApp.controller:AboutCtrl
+   * @description
+   * # AboutCtrl
+   * Controller of the segoEnnOgAfturApp
+   */
+  angular.module('segoApp')
+    .controller('HomeCtrl', ['$scope', 'dagatalFactory', '$http', function ($scope, dagatalFactory, $http) {
 
-        $scope.openBooking = function (a, b) {
-            if (a === undefined) {
-                console.log("UNDEFINED");
-            }
-            else {
-                console.log("bóka hjá " + b.name + " klukkan " + a);
-            }
+      $scope.openBooking = function (a, b) {
+        if (a === undefined) {
+          console.log("UNDEFINED");
+        }
+        else {
+          console.log("bóka hjá " + b.name + " klukkan " + a);
+        }
 
-        };
-        
- 
-        $http({
-            method: 'GET',
-            url: 'http://wwww.sego.is:6969/api/booking',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-        }}).then(function (response) {
-              console.log("RESPONSE:" + response);
-        }, function(err) {
-            console.log("ERROR", JSON.stringify(err));
-        });
-
-        var stillingar = {
-            upphafsTimi: 3600*7,
-            endaTimi: 3600*22,
-            lotan: 900
-        };
-
-        $scope.times = [];
-
-        var initTimes = function() {
-            var i;
-            for (i = stillingar.upphafsTimi; i <= stillingar.endaTimi; i += stillingar.lotan) {
-                $scope.times.push(dagatalFactory.timasetning(i));
-            }
-        };
+      };
 
 
-        $scope.names = [ {
-        	'id':1,
-        	'name':'Einar'
-        }, {
-        	'id':2,
-        	'name':'Siggi'
-        }, {
-        	'id':3,
-        	'name':'Efro'
-        }, {
-        	'id':4,
-        	'name': 'Kaplo'
-        }, {
-        	'id':5,
-        	'name':'Ostaran'
-        }, {
-        	'id':6,
-        	'name': 'Mandalana'
-        }];
+      /*$http({
+        method: 'GET',
+        url: 'http://wwww.sego.is:6969/api/booking',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (response) {
+        console.log("RESPONSE:" + response);
+      }, function (err) {
+        console.log("ERROR", JSON.stringify(err));
+      });*/
 
-        $scope.bookings = [ {
-          'time': '8:00',
-          'name':'Einar'
-        }, {
-          'time': '9:00',
-          'name':'Siggi'
-        }, {
-          'time': '10:00',
-          'name':'Efro'
-        }, {
-          'time': '11:00',
-          'name': 'Kaplo'
-        }, {
-          'time': '12:00',
-          'name':'Ostaran'
-        }, {
-          'time': '13:00',
-          'name': 'Mandalana'
-        }];
+      var invocation = new XMLHttpRequest();
+      var url = 'http://www.sego.is:6969/api/booking';
+      function callOtherDomain() {
+        if (invocation) {
+          invocation.open('GET', url, true);
+          invocation.onreadystatechange = handler;
+          invocation.send();
+        }
+      }
 
-        $scope.$on('dagsetning', function(e, a) {
-          initTimes();
-          $scope.dagurinnIdag = dagatalFactory.dagsetning(a.getDay(), a.getDate(), a.getMonth());
-        });
-      }]);
+      $scope.callOtherDomain();
+
+      var stillingar = {
+        upphafsTimi: 3600 * 7,
+        endaTimi: 3600 * 22,
+        lotan: 900
+      };
+
+      $scope.times = [];
+
+      var initTimes = function () {
+        var i;
+        for (i = stillingar.upphafsTimi; i <= stillingar.endaTimi; i += stillingar.lotan) {
+          $scope.times.push(dagatalFactory.timasetning(i));
+        }
+      };
+
+
+      $scope.names = [{
+        'id': 1,
+        'name': 'Einar'
+      }, {
+        'id': 2,
+        'name': 'Siggi'
+      }, {
+        'id': 3,
+        'name': 'Efro'
+      }, {
+        'id': 4,
+        'name': 'Kaplo'
+      }, {
+        'id': 5,
+        'name': 'Ostaran'
+      }, {
+        'id': 6,
+        'name': 'Mandalana'
+      }];
+
+      $scope.bookings = [{
+        'time': '8:00',
+        'name': 'Einar'
+      }, {
+        'time': '9:00',
+        'name': 'Siggi'
+      }, {
+        'time': '10:00',
+        'name': 'Efro'
+      }, {
+        'time': '11:00',
+        'name': 'Kaplo'
+      }, {
+        'time': '12:00',
+        'name': 'Ostaran'
+      }, {
+        'time': '13:00',
+        'name': 'Mandalana'
+      }];
+
+      $scope.$on('dagsetning', function (e, a) {
+        initTimes();
+        $scope.dagurinnIdag = dagatalFactory.dagsetning(a.getDay(), a.getDate(), a.getMonth());
+      });
+    }]);
 })();
