11a143808882d25ba1b2d5986744e479da95399c
Prumpu Rassgat <einaragusta@gmail.com>
2016-10-23 11:58:17 +0000

    breyting med authorization i header

--- app/scripts/controllers/homeCtrl.js
+++ app/scripts/controllers/homeCtrl.js
@@ -26,7 +26,8 @@
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('id_token')
         }}).then(function (response) {
               console.log("RESPONSE:" + response);
         }, function(err) {
