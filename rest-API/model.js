bd424746e3b07e68281a96cab4689503f1fcf9a1
oliivar <olafurij12@ru.is>
2016-10-24 20:03:46 +0000

    setti inn i model.js

---
+++ rest-API/model.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const personaSchema = mongoose.Schema({
+  persona_id: String,
+  name: String,
+  address: String,
+  phone: Number,
+  image_url: String
+});
+
+const companySchema = mongoose.Schema({
+  company_id: String,
+  name: String,
+  phone: Number,
+  address: [String],
+  logo_url: String,
+  staff: [{
+    persona_id: String,
+    role: Number
+  }]
+});
+
+const bookingsSchema = mongoose.Schema({
+  company_id: String,
+  date: Date,
+  bookings: [{
+    customer_id: String,
+    staff_id: String,
+    time: Date
+  }]
+});
+
+const customerSchema = mongoose.Schema({
+  persona_id: String,
+  company_id: String,
+  history: [{
+    String
+  }]
+});
+
+const serviceSchema = mongoose.Schema({
+  company_id: String,
+  pricelist: [{
+    name: String,
+    price: Number
+  }]
+});
