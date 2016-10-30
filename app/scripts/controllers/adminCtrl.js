6f2f4430f6b91b8b44699b0bf288ec6b084cae54
einarprump <einara12@ru.is>
2016-10-30 19:22:31 +0000

    AdminCtrl og Adming View tengdur vid REST get/post

--- app/scripts/controllers/adminCtrl.js
+++ app/scripts/controllers/adminCtrl.js
@@ -4,6 +4,7 @@
     angular.module('segoApp')
       .controller('AdminCtrl', ['$scope', '$http', function ($scope, $http) {
             var profile = JSON.parse(localStorage.getItem('profile'));
+            // GET ALL COMPANIES //
             
             $http({
                 url: 'http://wwww.sego.is:6969/api/companies',
@@ -14,17 +15,36 @@
                     'Authorization': 'Bearer ' + localStorage.getItem('id_token')
                 }
             }).then(function (response) {
-                $scope.companies = response;
+                $scope.companies = response.data;
                 console.log("RESPONSE:", response);
             }).catch(function(err) {
                 console.log("ERROR", JSON.stringify(err));
             }).finally(function() {} );
             
+            // END GETTING ALL //
+            
+            // GET ALL USERS //
+            $http({
+                url: 'http://wwww.sego.is:6969/api/persons',
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('id_token')
+                }
+            }).then(function (response) {
+                $scope.users = response.data;
+                console.log("RESPONSE:", response);
+            }).catch(function(err) {
+                console.log("ERROR", JSON.stringify(err));
+            }).finally(function() {} );
+            // END GETTING ALL //
+            
             // WHEN CREATING NEW COMPANY
             $scope.company = {};
             $scope.company.auth_id = profile.user_id;
             
-            $scope.stadfesta = function(c) {
+            $scope.addCompany = function(c) {
                 $http({
                 url: 'http://wwww.sego.is:6969/api/companies',
                 method: 'POST',
@@ -42,6 +62,27 @@
             }
             // END CREATING COMPANY
             
+            // WHEN CREATING NEW USER
+            $scope.user = {};
+            
+            $scope.addUser = function(u) {
+                $http({
+                url: 'http://wwww.sego.is:6969/api/persons',
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('id_token')
+                },
+                data: u
+            }).then(function (response) {
+                console.log("RESPONSE:", response);
+            }).catch(function(err) {
+                console.log("ERROR", JSON.stringify(err));
+            }).finally(function() {} );
+            }
+            
+            // END CREATING USER
             
       }]);
 })();
