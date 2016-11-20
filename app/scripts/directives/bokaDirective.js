dfe5da5a4ae1ddb997789a2ebec8137c3f42c759
stefand12 <stefand12@ru.is>
2016-11-20 15:39:37 +0000

    service in checkbox

--- app/scripts/directives/bokaDirective.js
+++ app/scripts/directives/bokaDirective.js
@@ -29,6 +29,12 @@
                 scope.close();
             };
 
+          backendFactory.getService().then(function(res) {
+            scope.pricelist = res.data[0].pricelist;
+          }, function(err) {
+            console.log("ERROR getStaf(): ", err);
+          });
+          scope.serviceCheckbox = {};
 
             scope.stadfesta = function(bokun) {
 
