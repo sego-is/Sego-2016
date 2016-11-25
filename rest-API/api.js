ae47ddfc98fd1a6d0a9cb198c769a56170c97d9c
einarprump <einara12@ru.is>
2016-11-25 23:46:05 +0000

    tilrauning allt i attina

--- rest-API/api.js
+++ rest-API/api.js
@@ -159,7 +159,7 @@
   });
 
   api.get('/bookings/:date/:id', (req, res) => {
-     model.Booking.find({ company_id: req.params.id, date: req.params.date}).populate('bookings.customer_id').exec(function (err, docs) {
+     model.Booking.find({ company_id: req.params.id, date: req.params.date}).populate('bookings.book_id').exec(function (err, docs) {
         if (err) {
             res.status(500).send(err);
         } else {
