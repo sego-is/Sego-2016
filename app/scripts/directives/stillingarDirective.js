3b1a0889563f2f9bead9a7f5e9eeb2934d255e67
einarprump <einara12@ru.is>
2016-12-04 20:48:58 +0000

    api breytingar get booking by month or else?

--- app/scripts/directives/stillingarDirective.js
+++ app/scripts/directives/stillingarDirective.js
@@ -66,8 +66,9 @@
             });
           };
 
-          scope.toggleStaffActive = function () {
-            console.log("staff active/!active");
+          scope.toggleStaffActive = function (ev) {
+              
+            console.log("toggleStaffActive: $event:", ev);
           };
 
           scope.toggleView = function () {
