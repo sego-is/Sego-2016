1168d16b3d87a01c5d63aea398de3532ef50ba1b
einarprump <einara12@ru.is>
2016-12-05 20:44:58 +0000

    api, onnur tilraun

--- rest-API/api.js
+++ rest-API/api.js
@@ -233,7 +233,7 @@ api.get('/book/:cid/:pid', (req, res) => {
                      for (var i in docs) {
                         tmpDoc.push(_.where(docs[i].bookings, { staff_id: req.params.pid }));    
                      }
-                     console.log("get('/bookings/:cid/:pid/:date, tmpDoc:", tmpDoc);
+                     console.log("get('/bookings/:cid/:pid/:date, tmpDoc:", docs[0].bookings);
                      res.send(tmpDoc);
                     
                 }
